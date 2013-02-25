2b187fc45c31e1b0a5a54a437a2c6cb5adf7d9ef
unknown <marin.masic@HR_MASICM.snt-is.com>
2013-02-25 14:03:04 +0100

    RacunEdit sređena KO validacija

--- NinjaSoftware.Enio/App_Start/BundleConfig.cs
+++ NinjaSoftware.Enio/App_Start/BundleConfig.cs
@@ -36,7 +36,9 @@ namespace NinjaSoftware.Enio
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css", "~/Content/Custom.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/site.css", 
+                "~/Content/Custom.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
